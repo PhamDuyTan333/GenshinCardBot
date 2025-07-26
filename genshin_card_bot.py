1dacb4dae755f7f1ec11a41543d9fa8a4185b691
PhamDuyTan333 <kk3504642@gmail.com>
2025-07-26 20:38:15 +0700

    up 1

--- genshin_card_bot.py
+++ genshin_card_bot.py
@@ -18,12 +18,12 @@ bot = commands.Bot(command_prefix="/", intents=intents)
 # Danh sách thẻ bài Genshin Impact
 CARDS = [
     # Thẻ nhân vật
-    {"name": "Arlecchino", "type": "character", "power": 90, "element": "Hỏa", "rarity": "Huyền Thoại", "image": "https://genshin.honeyhunterworld.com/img/char/arlecchino_035.webp"},
-    {"name": "Diluc", "type": "character", "power": 85, "element": "Hỏa", "rarity": "Huyền Thoại", "image": "https://genshin.honeyhunterworld.com/img/char/diluc_002.webp"},
-    {"name": "Keqing", "type": "character", "power": 80, "element": "Lôi", "rarity": "Hiếm", "image": "https://genshin.honeyhunterworld.com/img/char/keqing_007.webp"},
-    {"name": "Fischl", "type": "character", "power": 70, "element": "Lôi", "rarity": "Hiếm", "image": "https://genshin.honeyhunterworld.com/img/char/fischl_012.webp"},
-    {"name": "Noelle", "type": "character", "power": 65, "element": "Nham", "rarity": "Thường", "image": "https://genshin.honeyhunterworld.com/img/char/noelle_014.webp"},
-    {"name": "Barbara", "type": "character", "power": 60, "element": "Thủy", "rarity": "Thường", "image": "https://genshin.honeyhunterworld.com/img/char/barbara_005.webp"},
+    {"name": "Arlecchino", "type": "character", "power": 90, "element": "Hỏa", "rarity": "Huyền Thoại", "image": "https://hoanghamobile.com/tin-tuc/wp-content/uploads/2024/11/Arlecchino.jpg"},
+    {"name": "Diluc", "type": "character", "power": 85, "element": "Hỏa", "rarity": "Huyền Thoại", "image": "https://game8.vn/media/202206/images/3006-rumor-diluc-fischl-skin-price-special-effects-release-date-genshin-impact-2-8-leaks_5.PNG"},
+    {"name": "Keqing", "type": "character", "power": 80, "element": "Lôi", "rarity": "Hiếm", "image": "https://images7.alphacoders.com/129/1295208.jpg"},
+    {"name": "Fischl", "type": "character", "power": 70, "element": "Lôi", "rarity": "Hiếm", "image": "https://blog.rrqtopup.com/wp-content/uploads/2025/03/Fischl_Birthday_2023-1000x768.webp"},
+    {"name": "Noelle", "type": "character", "power": 65, "element": "Nham", "rarity": "Thường", "image": "https://upload-os-bbs.hoyolab.com/upload/2024/03/21/238848438/7e36c698a0fe48d7573fa0ea0572ddfa_3253550726833357605.jpg?x-oss-process=image%2Fresize%2Cs_1000%2Fauto-orient%2C0%2Finterlace%2C1%2Fformat%2Cwebp%2Fquality%2Cq_70"},
+    {"name": "Barbara", "type": "character", "power": 60, "element": "Thủy", "rarity": "Thường", "image": "https://lensmate.co.uk/cdn/shop/files/barbara.jpg?v=1714394489"},
     # Thẻ hành động
     {"name": "Paimon", "type": "action", "power": 10, "element": "None", "rarity": "Thường", "image": "https://genshin.honeyhunterworld.com/img/char/paimon_099.webp", "effect": "Tăng 10 sát thương cho thẻ nhân vật"},
     {"name": "Tiên Nhảy Tường", "type": "action", "power": 15, "element": "None", "rarity": "Hiếm", "image": "https://genshin.honeyhunterworld.com/img/item/delicious_jade_parcels_510.webp", "effect": "Tăng 15 sát thương cho toàn đội"},
